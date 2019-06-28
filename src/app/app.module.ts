066466917fc162bde0277579d64e69764d957fbe
Jose Andres Lopez Castaño <42095301+joselopez1913@users.noreply.github.com>
2019-06-27 23:07:18 -0500

    paso6 HTTP2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,32 @@ import { AppRoutingModule }     from './app-routing.module';
 import { HttpClientModule }    from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+ 
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
+  
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HttpClientModule,
-    HeroSearchComponent 
+    HeroSearchComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
